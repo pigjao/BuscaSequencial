3b9c004339063a7c0538b920c8f07eeae80e5601
Pigjao <joaovictorbagomes@gmail.com>
2022-11-09 12:57:16 -0300

    desafioIfood

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner leitor = new Scanner(System.in);
+
+        int[] elementos = {64, 137, -16, 43, 67, 81, -90, 212, 10, 75};
+
+
+        int cont = 0;
+
+
+        int busca = leitor.nextInt();
+        for(int i = 0; i < elementos.length; i++){
+            if(elementos[i] == busca){
+                System.out.println("Achei " +busca+ " na posicao "+i);
+            break;
+            } else if(elementos.length == (i+1)){
+                System.out.println("Numero "+busca+" nao encontrado!");
+            }
+
+
+        }
     }
 }
